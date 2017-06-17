9c5a1d428407a28fe1d6ed76c40ec1ba2f2e265d
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-17 11:07:31 -0500

    generate products controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'products/index'
+
+  get 'products/new'
+
+  get 'products/create'
+
+  get 'products/show'
+
+  get 'products/edit'
+
+  get 'products/update'
+
+  get 'products/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
