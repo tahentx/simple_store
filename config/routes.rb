0e1d95e005a1eee47e105fd0ea8e21a18a608a3d
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-17 13:44:11 -0500

    show action implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
   root 'products#index'
 
+  get 'products/:id', to: 'products#show', as: 'product'
+
   get 'products', to: 'products#index', as: 'products'
 
   get 'products/new'
