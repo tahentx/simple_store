58f7907acfa6b8b644dcf8fb2f1a0f42e076e793
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-18 12:51:05 -0500

    add validation logic to product class in order to restrict user input type, also updated controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,4 +32,5 @@ class ProductsController < ApplicationController
   private
   def product_params
     params.require(:product).permit(:title, :description, :price, :stock_quantity)
+  end 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+	validates :title, :price, :stock_quantity, presence: true
+	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+	validates :stock_quantity, numericality: { only_integer: true }
 end
