27b1a374e6bb388524346f68d5498746f91f1276
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-19 09:56:54 -0500

    add uuid before_action to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,13 @@
 class Product < ApplicationRecord
+	before_create :create_uuid
 	validates :title, :price, :stock_quantity, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0.01 }
 	validates :stock_quantity, numericality: { only_integer: true }
+
+	private
+	def create_uuid
+	    begin
+	      self.uuid = SecureRandom.uuid
+	    end while self.class.exists?(:uuid => uuid)
+    end
 end
