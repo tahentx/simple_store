661f4cbecb73b8423edc3ebb52d40e13d022daeb
Todd Hendricks <hendricks.ta@gmail.com>
2017-06-19 15:00:21 -0500

    update destroy method to find_by instead of find

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -34,7 +34,7 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
+    @product = Product.find_by(uuid: params[:id])
     @product.destroy
     redirect_to root_path
   end
